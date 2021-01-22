c6c528693765151e89d5bf722372a5e3e104a1d9
Magnus Holta <magnus.holta@gmail.com>
2021-01-22 17:13:23 +0100

    CPT-3:  fix sheet to CP script

--- scripts/cpToSheet.js
+++ scripts/cpToSheet.js
@@ -1,77 +1,31 @@
-/*
- * Copyright (c) 2021 Magnus Holta <magnus.holta@gmail.com>
-
- Based on work of
- * Copyright (c) 2020 David Gulaker <dgulaker@hotmail.com>
- * Copyright (c) 2014-16 Greg Schoppe <gschoppe@gmail.com>
- * Copyright (c) 2011 Jonathan Perkin <jonathan@perkin.org.uk>
- *
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-//
-
-const all_keys = [
-  'A',
-  'Bb',
-  'B',
-  'Cb',
-  'C',
-  'C#',
-  'Db',
-  'D',
-  'Eb',
-  'E',
-  'F',
-  'F#',
-  'Gb',
-  'G',
-  'Ab',
-];
-
-const isChord = (word) => {
-  let isChord = false;
-  if (word)
-    all_keys.forEach((chord) => {
-      if (word.startsWith(chord) || word.startsWith('Aadd')) {
-        isChord = true;
-        return;
-      }
-    });
-  return isChord;
+const isCPOneLiner = (line) => {
+  const items = line.split(' ');
+  for (let i = 0; i < items.length; i++) {
+    let chord = items[i];
+    if (!(chord.startsWith('[') && chord.endsWith(']'))) return false;
+  }
+  return true;
 };
 
-const spaces = (number) => {
-  return ' '.repeat(number);
-};
+//console.log(isCPOneLiner('[|] [B] [/] [/] [/] [|] [Bsus] [/] [/] [/] [|]'));
 
-const cpToSheet = (
-  template,
-  key = undefined,
-  mode = 0,
-  transpose = 0,
-  table = false
-) => {
+const cpToSheet = (template) => {
   template = template.trim();
 
   var chordRegex = /\[([^\]]*)\]/;
   const transposedKey = '';
   const buffer = [];
 
-  template.split('\n').forEach((line, linenum) => {
-    /* If line has chords */
-    if (line.trim().match(chordRegex)) {
+  template.split('\n').forEach((line) => {
+    if (isCPOneLiner(line)) {
+      let oneLineBuffer = [];
+      console.log(line);
+      line.split(' ').forEach((chord) => {
+        oneLineBuffer.push(chord.unwrapChord());
+      });
+      console.log(oneLineBuffer);
+      buffer.push(oneLineBuffer.join(' '));
+    } /* If line has chords */ else if (line.trim().match(chordRegex)) {
       let chordLine = '';
       let chordLength = 0;
       let lyricLine = '';
---
+++ scripts/sheetToCp.js
@@ -0,0 +1,111 @@
+const isChordLine = (line) => {
+  // Get each word in line as a element in list
+  let lineWords = line.replace(/\s+/g, ' ').trim().split(' ');
+  for (let i = 0; i < lineWords.length; i++) {
+    let word = lineWords[i];
+    if (!isChord(word)) {
+      if (!isAcceptedOneLineParts(word)) return false;
+    }
+  }
+  return true;
+};
+
+const isAcceptedOneLineParts = (word) => {
+  const acceptedOneLineParts = ['|', '/', '-', '.', '(x', '(X', 'x', 'X'];
+  console.log(word);
+  for (let i = 0; i < acceptedOneLineParts.length; i++) {
+    let acceptedPart = acceptedOneLineParts[i];
+    if (word.startsWith(acceptedPart)) return true;
+  }
+  return false;
+};
+
+const checkAndFixOneLiner = (line) => {
+  line = line.trim().replace(/  +/g, ' ');
+  /* Supporting |chord, ||, --, .., // (and combinations) */
+  const regex = /(((\-|\.|\||\/)(C|D|E|F|G|A|B|\d)))|((\-|\.|\||\/){2})/gi;
+  let fixIndex;
+  fixIndex = line.search(regex);
+  if (fixIndex !== -1) {
+    while ((line.match(regex) || []).length > 0) {
+      fixIndex = line.search(regex);
+      line = line.splice(fixIndex + 1, ' ');
+    }
+  }
+  return line;
+};
+
+const isOneLiner = (line) => {
+  return line.trim().search(/\|/gi) !== -1;
+};
+
+const getChordsList = (line) => {
+  const chordsList = [];
+  const regex = /[^\s-]/;
+  let index;
+  let chord;
+  let i;
+  index = line.search(regex);
+  while (line.search(regex) !== -1) {
+    index = line.search(regex);
+    chord = '';
+    i = 0;
+    while (index + i < line.length && line[index + i] !== ' ') {
+      chord += line[index + i];
+      line = line.erase(index + i);
+      i++;
+    }
+    chordsList.push({
+      chord: chord,
+      index: index,
+    });
+  }
+  return chordsList;
+};
+
+const sheetToCp = (template) => {
+  const lines = template.split('\n');
+  const buffer = [];
+  let justMergedWithLyrics = false;
+  let passedMetaSection = false;
+
+  lines.forEach((line, linenum) => {
+    if (!passedMetaSection) {
+      buffer.push(line);
+      if (line.trim() === '') passedMetaSection = true;
+    } else if (!justMergedWithLyrics) {
+      if (line.trim() !== '' && isChordLine(line)) {
+        /* One liner */
+        if (isOneLiner(line)) {
+          const oneLineBuffer = [];
+          let list = checkAndFixOneLiner(line).split(' ');
+          list.forEach((element) => {
+            oneLineBuffer.push(element.wrapChord());
+          });
+          line = oneLineBuffer.join(' ');
+        } else {
+          /* Regular chord over text */
+          const nextLine = lines[linenum + 1] ?? '';
+          if (!isChordLine(nextLine)) {
+            const chords = getChordsList(line);
+            let lyricsLine = nextLine;
+            /* Adding chords backwards */
+            chords.reverse().forEach((chordObj) => {
+              lyricsLine = lyricsLine.splice(
+                chordObj.index,
+                chordObj.chord.wrapChord()
+              );
+            });
+            line = lyricsLine;
+            justMergedWithLyrics = true;
+          }
+        }
+      }
+      buffer.push(line);
+    } else {
+      justMergedWithLyrics = false;
+    }
+  });
+
+  return buffer.join('\n');
+};
